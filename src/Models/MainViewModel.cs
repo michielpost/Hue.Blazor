b7bf2a7357979f2e064e109a7f97bf86cc3cec82
michielpost <michiel@q42.nl>
2020-11-06 12:34:21 +0100

    Show groups and lights

---
+++ src/Models/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hue.Blazor.Models
+{
+    public class MainViewModel
+    {
+        public List<GroupViewModel> Groups { get; set; } = new List<GroupViewModel>();
+    }
+
+    public class GroupViewModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public List<LightViewModel> Lights { get; set; } = new List<LightViewModel>();
+    }
+
+    public class LightViewModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public bool IsOn { get; set; }
+
+        public string Color { get; set; }
+    }
+}
